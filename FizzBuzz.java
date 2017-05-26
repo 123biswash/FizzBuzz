d9da88713ce05e905c7dce36cef85af40e7c0b15
123biswash <biswash.adhikari@hotmail.com>
2017-05-26 13:32:56 -0400

    first commit

---
+++ FizzBuzz.java
@@ -0,0 +1,23 @@
+package FizzBuzz;
+
+import java.util.*;
+
+public class FizzBuzz {
+
+	public static void main(String[] args) {
+		int num=1;
+		while(num<=100){
+			if (num%3==0 &&num%5==0){
+				System.out.println("FizzBuzz");
+			}else if (num%3==0){
+				System.out.println("Fizz");
+			}else if(num%5==0){
+				System.out.println("Buzz");
+			}else{
+				System.out.println(num);
+			}
+			num++;
+		}
+	}
+
+}
